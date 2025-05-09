632b1601ab11b4b93b734af6677ae36ed7b8da77
rohrsa <rohrsa@sonoma.edu>
2025-05-09 12:51:43 -0700

    Update julia.cu

--- Project-4/julia.cu
+++ Project-4/julia.cu
@@ -188,7 +188,7 @@ void julia(Complex d, Complex center, Color* pixels) {
 
     Complex c(x * d.x, y * d.y);
     c -= center;
-    Complex z(0.355,0.355);
+    Complex z(0.9,1.0);
     
     int iter = 0;
     while (iter < MaxIterations && z.magnitude() < 2.0) {
